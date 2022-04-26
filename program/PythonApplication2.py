c30d90b7ec01c3f6d7ba8cc5b135161362a73fa2
pythonxjava <mohammed001@live.se>
2022-04-26 14:34:53 +0100

    cool

---
+++ program/PythonApplication2.py
@@ -0,0 +1,27 @@
+runescape = "runescape"
+league_of_legends = "league of legends"
+call_of_duty = "call of duty"
+user = input("guess the best game in history ")
+
+if user == "runescape":
+
+ print("correct!!")
+
+elif user == "league of legends":
+
+ print("ehh")
+ 
+elif user == "call of duty":
+
+ print("ehhh")
+
+else:
+
+ print("invalid!!!")
+
+
+
+
+
+
+
